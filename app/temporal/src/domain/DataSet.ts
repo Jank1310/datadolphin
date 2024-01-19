f7a9dd36655136f7f441d74d47ca877232ac1c47
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-01-19 15:15:40 +0100

    feat: implement a basic importer entity workflow #3

---
+++ app/temporal/src/domain/DataSet.ts
@@ -0,0 +1,5 @@
+export interface DataSetPatch {
+  row: number;
+  col: number;
+  newValue: string | number | null;
+}
--- app/temporal/src/workflows/importer.workflow.ts
+++ app/temporal/src/workflows/importer.workflow.ts
@@ -1,15 +1,92 @@
+import {
+  ApplicationFailure,
+  condition,
+  defineSignal,
+  setHandler,
+} from "@temporalio/workflow";
 import { ColumnConfig } from "../domain/ColumnConfig";
+import { DataSetPatch } from "../domain/DataSet";
 
 export interface ImporterWorkflowParams {
   columnConfig: ColumnConfig[];
   callbackUrl: string;
+  /**
+   * Timeout for upload of file.
+   * If not set, defaults to 24 hours.
+   */
+  uploadTimeout?: string;
+  /**
+   * Timeout for the start of the import.
+   * If not set, defaults to 24 hours.
+   */
+  startImportTimeout?: string;
 }
+
+const addFileSignal = defineSignal<
+  [
+    {
+      fileReference: string;
+      fileFormat: "csv" | "xlsx";
+    }
+  ]
+>("importer:add-file");
+const addPatchesSignal =
+  defineSignal<[{ patches: DataSetPatch[] }]>("importer:add-patch");
+const startImportSignal = defineSignal<[]>("importer:start-import");
 
 /**
  * Entity workflow which represents a complete importer workflow
  */
 export async function importer(params: ImporterWorkflowParams) {
-  console.log("importer workflow started");
+  const uploadTimeout = params.uploadTimeout ?? "24 hours";
+  const startImportTimeout = params.startImportTimeout ?? "24 hours";
+  const compensations: Function[] = [];
 
-  // TODO: implement workflow
+  let sourceFile: {
+    fileReference: string;
+    fileFormat: "csv" | "xlsx";
+  } | null = null;
+  let patches: DataSetPatch[] = [];
+  let importStartRequested = false;
+  setHandler(addFileSignal, (params) => {
+    sourceFile = {
+      fileReference: params.fileReference,
+      fileFormat: params.fileFormat,
+    };
+  });
+  setHandler(addPatchesSignal, (params) => {
+    patches.push(...params.patches);
+  });
+  setHandler(startImportSignal, () => {
+    importStartRequested = true;
+  });
+
+  try {
+    const hasSourceFile = await condition(
+      () => sourceFile !== null,
+      uploadTimeout
+    );
+    if (!hasSourceFile) {
+      throw ApplicationFailure.nonRetryable(
+        "Timeout: source file not uploaded"
+      );
+    }
+    // TODO add compensation to delete file from storage
+    // TODO analyze file and return file reference with dataset result
+    const hasImportStartRequested = await condition(
+      () => importStartRequested === true,
+      startImportTimeout
+    );
+    if (!hasImportStartRequested) {
+      throw ApplicationFailure.nonRetryable(
+        "Timeout: import start not requested"
+      );
+    }
+    // TODO perform import
+  } catch (err) {
+    for (const compensation of compensations) {
+      await compensation();
+    }
+    throw err;
+  }
 }
