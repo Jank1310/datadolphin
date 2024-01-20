a452cfbbaf1d26b80b945090a799f5a828b66879
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-01-20 11:53:57 +0100

    feat: support xlsx file

--- app/temporal/src/activities.ts
+++ app/temporal/src/activities.ts
@@ -1,5 +1,8 @@
+import { ApplicationFailure } from "@temporalio/workflow";
 import csv from "csv";
+import XLSX from "xlsx";
 import { FileStore } from "./infrastructure/FileStore";
+
 export interface DownloadSourceFileParams {
   filename: string;
   importerId: string;
@@ -26,6 +29,7 @@ export function makeActivities(fileStore: FileStore) {
         params.bucket,
         params.fileReference
       );
+      let jsonData: Buffer;
       switch (params.format) {
         case "csv":
           const rows = await new Promise<Record<string, string>[]>(
@@ -48,11 +52,27 @@ export function makeActivities(fileStore: FileStore) {
             }
           );
           console.log("received rows", rows.length);
-          const data = Buffer.from(JSON.stringify(rows));
-          fileStore.putFile(params.bucket, params.outputFileReference, data);
+          jsonData = Buffer.from(JSON.stringify(rows));
           break;
         case "xlsx":
+          const workbook = XLSX.read(fileData, { type: "buffer" });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const json = XLSX.utils.sheet_to_json(worksheet);
+          console.log("received rows", json.length);
+          console.log(json);
+          jsonData = Buffer.from(JSON.stringify(json));
+          break;
+        default:
+          throw ApplicationFailure.nonRetryable(
+            `Unsupported format ${params.format}`
+          );
       }
+      await fileStore.putFile(
+        params.bucket,
+        params.outputFileReference,
+        jsonData
+      );
     },
   };
 }
