c649221bbbc9966b1d294d96c1ea0d477d862b98
Florian <fiedlefl@gmail.com>
2024-01-24 08:36:20 +0100

    chore(app): cache regex results

--- app/temporal/src/domain/validators/RegexValidator.ts
+++ app/temporal/src/domain/validators/RegexValidator.ts
@@ -6,14 +6,25 @@ export class RegexValidator {
     columnConfig: { column: string; regex?: string }[]
   ): Record<string, ValidationError> {
     const errors: Record<string, ValidationError> = {};
+    const cache: Record<string, boolean> = {};
     for (const config of columnConfig) {
       const { column: columnToValidate, regex } = config;
       let dataToValidate = row[columnToValidate];
-      if (regex && new RegExp(regex).test(dataToValidate as string) === false) {
+      if (cache[columnToValidate] === false) {
         errors[columnToValidate] = {
           type: "regex",
           message: `value does not match regex ${regex}`,
         };
+      } else {
+        const regexTestResult =
+          regex && new RegExp(regex).test(dataToValidate as string);
+        cache[columnToValidate] = regexTestResult as boolean;
+        if (regexTestResult === false) {
+          errors[columnToValidate] = {
+            type: "regex",
+            message: `value does not match regex ${regex}`,
+          };
+        }
       }
     }
     return errors;
