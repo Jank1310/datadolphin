41799784d6e729fec2e58b262330dd94ba26dd6a
Florian <fiedlefl@gmail.com>
2024-01-31 08:43:23 +0100

    feat(app): capsule database calls

--- app/temporal/src/activities.ts
+++ app/temporal/src/activities.ts
@@ -1,7 +1,7 @@
 import { ApplicationFailure } from "@temporalio/workflow";
 import csv from "csv";
 import { pull } from "lodash";
-import { AnyBulkWriteOperation, ObjectId } from "mongodb";
+import { ObjectId } from "mongodb";
 import XLSX from "xlsx";
 import { ColumnConfig } from "./domain/ColumnConfig";
 import {
@@ -99,14 +99,10 @@ export function makeActivities(
       );
       console.log("insert rows", jsonWithRowIds.length);
       console.time("insert");
-      await database.mongoClient
-        .db(params.importerId)
-        .collection("sourceData")
-        .deleteMany();
-      const result = await database.mongoClient
-        .db(params.importerId)
-        .collection("sourceData")
-        .insertMany(jsonWithRowIds);
+      const result = await database.insertSourceData(
+        params.importerId,
+        jsonWithRowIds
+      );
       console.timeEnd("insert");
       if (result.insertedCount !== jsonWithRowIds.length) {
         throw ApplicationFailure.nonRetryable("Failed to insert all rows");
@@ -117,16 +113,8 @@ export function makeActivities(
       dataMapping: Mapping[];
     }): Promise<void> => {
       // drop collection for idempotency
-      await database.mongoClient
-        .db(params.importerId)
-        .collection("data")
-        .drop();
-      const sourceJsonData: SourceDataSet = await database.mongoClient
-        .db(params.importerId)
-        .collection<SourceDataSetRow>("sourceData")
-        .find()
-        .toArray();
-
+      await database.dropDataCollection(params.importerId);
+      const sourceJsonData = await database.getSourceData(params.importerId);
       console.log("got sourceData", sourceJsonData.length);
       // stats
       // {name: {messageCount: 104}}
@@ -149,43 +137,20 @@ export function makeActivities(
       });
 
       console.log("creating indexes");
-      await database.mongoClient
-        .db(params.importerId)
-        .collection("data")
-        .createIndexes([
-          {
-            key: {
-              "data.$**": 1,
-            },
-            name: "data_1",
-          },
-          {
-            key: {
-              __sourceRowId: 1,
-            },
-            name: "sourceRowId_1",
-            unique: true,
-          },
-        ]);
+      await database.createIndexes(params.importerId);
 
       console.time("writing data");
-      await database.mongoClient
-        .db(params.importerId)
-        .collection("data")
-        .insertMany(mappedData);
+      await database.saveData(params.importerId, mappedData);
       console.timeEnd("writing data");
     },
     getMappingRecommendations: async (params: {
       importerId: string;
       columnConfig: ColumnConfig[];
     }): Promise<DataMappingRecommendation[]> => {
-      const firstRecord = await database.mongoClient
-        .db(params.importerId)
-        .collection<DataSetRow>("sourceData")
-        .findOne();
+      const firstRecord = await database.getFirstSourceRow(params.importerId);
       // all rows should have all available headers (see source file processing)
       const sourceColumns = pull(
-        Object.keys(firstRecord as DataSetRow),
+        Object.keys(firstRecord as SourceDataSetRow),
         "__sourceRowId"
       );
       return dataAnalyzer.generateMappingRecommendations(
@@ -200,13 +165,11 @@ export function makeActivities(
       skip: number;
       limit: number;
     }): Promise<number> => {
-      const jsonData: DataSet = await database.mongoClient
-        .db(params.importerId)
-        .collection<DataSetRow>("data")
-        .find()
-        .skip(params.skip)
-        .limit(params.limit)
-        .toArray();
+      const jsonData: DataSet = await database.getData(
+        params.importerId,
+        params.skip,
+        params.limit
+      );
 
       const validationResults = dataAnalyzer.processDataValidations(
         jsonData,
@@ -214,27 +177,10 @@ export function makeActivities(
         params.stats
       );
 
-      const writes: AnyBulkWriteOperation<Document>[] = [];
-      for (const validationResult of validationResults) {
-        for (const message of validationResult.messages) {
-          writes.push({
-            updateOne: {
-              filter: {
-                _id: validationResult.rowId,
-              },
-              update: {
-                $addToSet: {
-                  [`data.${validationResult.column}.messages`]: message,
-                },
-              },
-            },
-          });
-        }
-      }
-      await database.mongoClient
-        .db(params.importerId)
-        .collection("data")
-        .bulkWrite(writes);
+      await database.updateDataWithValidationMessages(
+        params.importerId,
+        validationResults
+      );
       return validationResults.length;
     },
     applyPatches: async (params: {
@@ -242,21 +188,7 @@ export function makeActivities(
       patches: DataSetPatch[];
     }): Promise<void> => {
       for (const patch of params.patches) {
-        const targetColumn = `data.${patch.column}`;
-        await database.mongoClient
-          .db(params.importerId)
-          .collection("data")
-          .updateOne(
-            { __sourceRowId: patch.rowId },
-            {
-              $set: {
-                [targetColumn]: patch.newValue,
-                $addToSet: {
-                  history: patch,
-                },
-              },
-            }
-          );
+        await database.applyPatch(params.importerId, patch);
       }
     },
     generateStats: async (params: {
@@ -267,62 +199,18 @@ export function makeActivities(
       totalCount: number;
     }> => {
       console.time("generate-stats");
-      const $facet: Record<string, unknown> = {};
-      const $replaceRoot: Record<string, unknown> = {};
-      for (const uniqueColumn of params.uniqueColumns) {
-        $facet[uniqueColumn] = [
-          {
-            $group: {
-              _id: `$data.${uniqueColumn}.value`,
-              count: { $sum: 1 },
-            },
-          },
-          {
-            $match: {
-              count: { $ne: 1 },
-            },
-          },
-          {
-            $project: {
-              _id: 0,
-              k: "$_id",
-              v: "$count",
-            },
-          },
-        ];
-        $replaceRoot[uniqueColumn] = {
-          nonuniqe: {
-            $arrayToObject: `$${uniqueColumn}`,
-          },
-        };
-      }
-      const stats = (
-        await database.mongoClient
-          .db(params.importerId)
-          .collection<DataSetRow>("data")
-          .aggregate<SourceFileStatsPerColumn>([
-            {
-              $facet: $facet,
-            },
-            {
-              $replaceRoot: {
-                newRoot: $replaceRoot,
-              },
-            },
-          ])
-          .toArray()
-      )[0];
+      const columnStats = await database.getStats(
+        params.importerId,
+        params.uniqueColumns
+      );
 
       console.timeEnd("generate-stats");
 
       console.time("total-count");
-      const totalCount = await database.mongoClient
-        .db(params.importerId)
-        .collection<DataSetRow>("data")
-        .countDocuments();
+      const totalCount = await database.getTotalCount(params.importerId);
       console.timeEnd("total-count");
 
-      return { columnStats: stats, totalCount };
+      return { columnStats, totalCount };
     },
     invokeCallback: async (params: {
       importerId: string;
@@ -337,17 +225,11 @@ export function makeActivities(
       });
     },
     createDatabases: async (params: { importerId: string }): Promise<void> => {
-      // drop databases
-      await database.mongoClient.db(params.importerId).dropDatabase();
-
-      await database.mongoClient.db(params.importerId).createCollection("data");
-      await database.mongoClient
-        .db(params.importerId)
-        .createCollection("sourceData");
-      await database.mongoClient.db(params.importerId).createCollection("meta");
+      await database.dropDatabases(params.importerId);
+      await database.createDatabases(params.importerId);
     },
     dropDatabase: async (params: { importerId: string }): Promise<void> => {
-      await database.mongoClient.db(params.importerId).dropDatabase();
+      await database.dropDatabases(params.importerId);
     },
   };
 }
