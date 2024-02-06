7fb1498596e50ff880251756e2003ec3b5e336fa
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-02-06 09:14:12 +0100

    fix: positioning of tooltip

--- app/src/components/ui/tooltip.tsx
+++ app/src/components/ui/tooltip.tsx
@@ -10,7 +10,7 @@ const TooltipProvider = TooltipPrimitive.Provider;
 const Tooltip = TooltipPrimitive.Root;
 
 const TooltipTrigger = TooltipPrimitive.Trigger;
-
+const TooltipPortal = TooltipPrimitive.Portal;
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
