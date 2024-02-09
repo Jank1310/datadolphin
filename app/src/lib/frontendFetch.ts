dc7a95757585cb1d3450223437163df0a752e7cb
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-02-09 18:01:29 +0100

    fix: correctly add authorization

--- app/src/lib/frontendFetch.ts
+++ app/src/lib/frontendFetch.ts
@@ -1,11 +1,9 @@
-import { cloneDeep, set } from "lodash";
-
 export async function fetchWithAuth(...args: Parameters<typeof fetch>) {
-  const internalArgs = cloneDeep(args);
-  set(
-    internalArgs,
-    "headers.Authorization",
-    "Bearer " + process.env.NEXT_PUBLIC_FRONTEND_TOKEN
-  );
-  return await fetch(...internalArgs);
+  return await fetch(args[0], {
+    ...args[1],
+    headers: {
+      authorization: "Bearer " + process.env.NEXT_PUBLIC_FRONTEND_TOKEN,
+      ...args[1]?.headers,
+    },
+  });
 }
