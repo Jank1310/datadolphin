d948e4640cb4e11feac8d65831dc67e91722ed82
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-02-09 17:39:10 +0100

    feat: add close route

---
+++ app/src/app/api/importer/[slug]/close/route.ts
@@ -0,0 +1,17 @@
+import { getTemporalWorkflowClient } from "@/lib/temporalClient";
+import { validateServerAuth } from "@/lib/validateAuth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  if (validateServerAuth(req) === false) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+  const { slug: importerId } = params;
+  const client = await getTemporalWorkflowClient();
+  const handle = client.getHandle(importerId);
+  await handle.executeUpdate("importer:close");
+  return NextResponse.json({});
+}
