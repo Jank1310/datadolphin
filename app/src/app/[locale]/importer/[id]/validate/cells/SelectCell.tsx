83eea4b4ae0042085edc55b5c1e6fe165342710e
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-02-24 21:26:27 +0100

    fix: correctly select none values in select cell

--- app/src/app/[locale]/importer/[id]/validate/cells/SelectCell.tsx
+++ app/src/app/[locale]/importer/[id]/validate/cells/SelectCell.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import { ChevronDown } from "lucide-react";
 import React, { useId } from "react";
+import { useTranslation } from "react-i18next";
 
 type Props = {
   value: string;
@@ -39,26 +40,40 @@ const SelectCell = ({
   availableValues,
   isReadOnly,
 }: Props) => {
+  const { t } = useTranslation();
   const id = useId();
   const [value, setValue] = React.useState<string>(initialValue);
   React.useEffect(() => {
     setValue(initialValue);
   }, [initialValue]);
+  const normalizedString = (value as string) ?? "";
+  const isValueEmpty = normalizedString === "";
   return (
     <Select
       disabled={isReadOnly}
-      value={(value as string) ?? ""}
+      value={normalizedString === "" ? "none" : normalizedString}
       onValueChange={(newValue) => {
-        if (value !== newValue) {
+        const normalizedNewValue = newValue === "none" ? "" : newValue;
+        if (value !== normalizedNewValue) {
           // use settimeout to get a snappier experience
-          setTimeout(() => onChange(newValue));
+          setTimeout(() => onChange(normalizedNewValue));
         }
         setValue(newValue);
       }}
     >
-      <SelectCellTrigger className="">{value}</SelectCellTrigger>
+      <SelectCellTrigger
+        className={cn({
+          "text-gray-400": isValueEmpty,
+        })}
+      >
+        {isValueEmpty ? t("validation.none") : value}
+      </SelectCellTrigger>
       <SelectContent>
-        {!isRequired && <SelectItem value={"none"}>None</SelectItem>}
+        {!isRequired && (
+          <SelectItem className="text-gray-400" value={"none"}>
+            {t("validation.none")}
+          </SelectItem>
+        )}
         {availableValues.map((selectableValue) => (
           <SelectItem
             value={selectableValue}
