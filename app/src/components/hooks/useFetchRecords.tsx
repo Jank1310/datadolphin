61701e251a8424c41405629f1d18d4eaa5623c12
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-03-03 14:05:17 +0100

    fix: don't use url for relative url in frontend #83

--- app/src/components/hooks/useFetchRecords.tsx
+++ app/src/components/hooks/useFetchRecords.tsx
@@ -2,27 +2,28 @@ import { SourceData } from "@/app/api/importer/[slug]/ImporterDto";
 import { useFrontendFetchWithAuth } from "@/lib/frontendFetch";
 
 export function useFetchRecords(importerId: string | null) {
-  const frontendFetch = useFrontendFetchWithAuth();
-  if (!importerId) {
-    return () => [];
-  }
-  return async (page: number, pageSize: number) => {
-    return fetchRecords(frontendFetch, importerId, page, pageSize);
-  };
+    const frontendFetch = useFrontendFetchWithAuth();
+    if (!importerId) {
+        return () => [];
+    }
+    return async (page: number, pageSize: number) => {
+        return fetchRecords(frontendFetch, importerId, page, pageSize);
+    };
 }
 
 export async function fetchRecords(
-  frontendFetch: ReturnType<typeof useFrontendFetchWithAuth>,
-  importerId: string,
-  page: number,
-  pageSize: number
+    frontendFetch: ReturnType<typeof useFrontendFetchWithAuth>,
+    importerId: string,
+    page: number,
+    pageSize: number
 ): Promise<SourceData[]> {
-  const getUrl = new URL(`/api/importer/${importerId}/records`);
-  getUrl.searchParams.append("page", page.toFixed());
-  getUrl.searchParams.append("pageSize", pageSize.toFixed());
-  const result = frontendFetch(getUrl, {
-    method: "GET",
-    cache: "no-cache",
-  }).then((res) => res.json() as Promise<{ records: SourceData[] }>);
-  return (await result).records;
+    const searchParams = new URLSearchParams();
+    searchParams.append("page", page.toFixed());
+    searchParams.append("pageSize", pageSize.toFixed());
+    const getUrl = `/api/importer/${importerId}/records?${searchParams.toString()}`;
+    const result = frontendFetch(getUrl, {
+        method: "GET",
+        cache: "no-cache",
+    }).then((res) => res.json() as Promise<{ records: SourceData[] }>);
+    return (await result).records;
 }
