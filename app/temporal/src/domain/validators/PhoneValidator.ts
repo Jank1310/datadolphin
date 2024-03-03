bd813e0db5af64892b5d2dbedda80382f8b6de61
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-03-03 14:55:03 +0100

    chore: use isValidPhoneNumber

--- app/temporal/src/domain/validators/PhoneValidator.ts
+++ app/temporal/src/domain/validators/PhoneValidator.ts
@@ -1,31 +1,30 @@
-import { isPossiblePhoneNumber } from "libphonenumber-js";
+import { isValidPhoneNumber } from "libphonenumber-js";
 import { PhoneColumnValidation } from "../ColumnValidation";
 import { DataSetRow } from "../DataSet";
 import { ValidationMessage } from "../ValidationMessage";
 
 export class PhoneValidator {
-  validate(
-    row: DataSetRow,
-    columnConfig: { column: string; config: PhoneColumnValidation }[]
-  ): Record<string, ValidationMessage> {
-    const errors: Record<string, ValidationMessage> = {};
-    for (const { column, config } of columnConfig) {
-      let dataToValidate = row.data[column].value;
-      const isEmptyValue = Boolean(dataToValidate) === false;
-      if (isEmptyValue) {
-        continue;
-      }
-      if (
-        typeof dataToValidate !== "string" ||
-        isPossiblePhoneNumber(dataToValidate, config.defaultCountry ?? "DE") ===
-          false
-      ) {
-        errors[column] = {
-          type: "phone",
-          message: "value is not a valid phone number",
-        };
-      }
+    validate(
+        row: DataSetRow,
+        columnConfig: { column: string; config: PhoneColumnValidation }[]
+    ): Record<string, ValidationMessage> {
+        const errors: Record<string, ValidationMessage> = {};
+        for (const { column, config } of columnConfig) {
+            let dataToValidate = row.data[column].value;
+            const isEmptyValue = Boolean(dataToValidate) === false;
+            if (isEmptyValue) {
+                continue;
+            }
+            if (
+                typeof dataToValidate !== "string" ||
+                isValidPhoneNumber(dataToValidate, config.defaultCountry ?? "DE") === false
+            ) {
+                errors[column] = {
+                    type: "phone",
+                    message: "value is not a valid phone number",
+                };
+            }
+        }
+        return errors;
     }
-    return errors;
-  }
 }
