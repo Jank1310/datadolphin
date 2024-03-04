a7f8da4420e6c252ee1acea591bb8ab2963acacc
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-03-04 14:01:36 +0100

    fix: Fileupload with non ascii filename does not work #86

--- app/src/app/api/upload/route.ts
+++ app/src/app/api/upload/route.ts
@@ -6,57 +6,51 @@ import { getImporterManager } from "@/lib/ImporterManager";
 import { getMinioClient } from "../../../lib/minioClient";
 
 export async function POST(req: NextRequest) {
-  const { importerId, bucket, destFileName, fileFormat } =
-    await handleFileUpload(req);
-  const importerManager = await getImporterManager();
-  await importerManager.addFile(importerId, destFileName, fileFormat, bucket);
-  return new Response(undefined, { status: 201 });
+    const { importerId, bucket, destFileName, fileFormat } = await handleFileUpload(req);
+    const importerManager = await getImporterManager();
+    await importerManager.addFile(importerId, destFileName, fileFormat, bucket);
+    return new Response(undefined, { status: 201 });
 }
 
 async function handleFileUpload(req: NextRequest) {
-  const formData = await req.formData();
-  const file: File | null = formData.get("file") as unknown as File;
-  const importerId = formData.get("importerId") as unknown as string;
-  const fileBuffer = Buffer.from(await file.arrayBuffer());
+    const formData = await req.formData();
+    const file: File | null = formData.get("file") as unknown as File;
+    const importerId = formData.get("importerId") as unknown as string;
+    const fileBuffer = Buffer.from(await file.arrayBuffer());
 
-  if (!importerId) {
-    throw new Error("importerId missing");
-  }
-  if (!fileBuffer) {
-    throw new Error("file missing");
-  }
+    if (!importerId) {
+        throw new Error("importerId missing");
+    }
+    if (!fileBuffer) {
+        throw new Error("file missing");
+    }
 
-  const bucket = importerId;
-  const bucketExists = await getMinioClient().bucketExists(bucket);
+    const bucket = importerId;
+    const bucketExists = await getMinioClient().bucketExists(bucket);
 
-  if (bucketExists === false) {
-    await getMinioClient().makeBucket(bucket);
-  }
+    if (bucketExists === false) {
+        await getMinioClient().makeBucket(bucket);
+    }
 
-  const metadata = {
-    "Content-Type": file.type,
-    FileName: file.name,
-    ImporterId: importerId,
-  };
+    const metadata = {
+        "Content-Type": file.type,
+        FileName: encodeURIComponent(file.name),
+        ImporterId: importerId,
+    };
 
-  const destFileName = `${randomUUID()}${extname(file.name)}`;
-  try {
-    await getMinioClient().putObject(
-      bucket,
-      destFileName,
-      fileBuffer,
-      metadata
-    );
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error uploading file");
-  }
-  const fileFormat = extname(file.name) === ".csv" ? "csv" : "xlsx";
+    const destFileName = `${randomUUID()}${extname(file.name)}`;
+    try {
+        await getMinioClient().putObject(bucket, destFileName, fileBuffer, metadata);
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error uploading file");
+    }
+    const fileFormat = extname(file.name) === ".csv" ? "csv" : "xlsx";
 
-  return {
-    importerId,
-    bucket,
-    destFileName,
-    fileFormat,
-  };
+    return {
+        importerId,
+        bucket,
+        destFileName,
+        fileFormat,
+    };
 }
